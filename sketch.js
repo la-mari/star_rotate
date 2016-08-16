0fa06b838e732fa3926c9cdcb54699a0654cc0a9
la-mari <karima19@gmail.com>
2016-08-15 21:52:10 -0700

    added nested loop

--- sketch.js
+++ sketch.js
@@ -4,17 +4,36 @@ var speed = 0.05;
 
 function setup(){
 	createCanvas(windowWidth, windowHeight);
-	fill(0);
-	strokeWeight(15);
+	noFill();
+	// fill(0);
+	// strokeWeight(15);
 }
 
 function draw(){
-	var arm = map(mouseX, 0, width, 10, 300);
+	var l0 = map(mouseX, 0, width, 10, 300);
 	background(255);
 
 	translate(width/2, height/2);
 	rotate(angle);
-	line(0, 0, 0, arm);
+	// line(0, 0, 0, l0);
+	for (var i=0; i<5; i++){
+		push();
+		rotate(i*TWO_PI/5);
+		translate(0, l0);
+		ellipse(0, 0, 20, 20);
+
+		rotate(angle);
+		for (var j=0; j<5; j++){
+			push();
+			rotate(j*TWO_PI/5);
+			translate(0, l0);
+			ellipse(0, 0, 20, 20);
+
+			pop();
+		}
+		pop();
+	}
+
 
 	angle += speed;
 }
